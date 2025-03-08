9f751e4d3637de11f88edf705769e782dc3542b4
jatin31195 <jatinsingh31185@gmail.com>
2025-03-09 01:27:35 +0530

    ADD: Implemented cron to request data as per time

--- Server/index.js
+++ Server/index.js
@@ -9,7 +9,13 @@ app.use(express.json());
 app.get("/", (req, res) => res.send("Server is running..."));
 
 app.use("/api",apiRoutes);
+const cron = require("node-cron");
+const { fetchAndStorePOTD } = require("./services/potdServices/fetchPOTDService");
 
+cron.schedule("*/30 * * * * *", async () => {
+    console.log("Fetching POTD every 30 seconds...");
+    await fetchAndStorePOTD();
+});
 app.listen(serverConfig.PORT, () => {
     console.log(`Successfully started the server on PORT : ${serverConfig.PORT}`);
 });
