8cff8e9bf58a3dcb06639a0c2cf0e742b6ec066d
jatin31195 <jatinsingh31185@gmail.com>
2025-05-17 17:35:34 +0530

    Added feature for User to create its own list of problem ,list can be private or public

--- Server/routes/potdRoutes/customUserPOTD-routes.js
+++ Server/routes/potdRoutes/customUserPOTD-routes.js
@@ -1,8 +1,22 @@
 const express = require("express");
-const { handleCustomUserPOTD } = require("../../controllers/potdControllers/customUserPOTD-controller");
-
 const router = express.Router();
+const authMiddleware = require("../../middlewares/authMiddlewares"); // Import auth middleware
 
-router.post("/add", handleCustomUserPOTD);
+const {
+  handleCreateList,
+  handleAddQuestion,
+  handleGetPublicLists,
+  handleGetQuestionsFromList,
+  handleUpdateListVisibility,
+  handleGetOwnLists, // Import new handler
+} = require("../../controllers/potdControllers/customUserPOTD-controller");
+
+// All routes that modify data should be protected by auth middleware
+router.post("/list", authMiddleware, handleCreateList);
+router.post("/list/add-question", authMiddleware, handleAddQuestion);
+router.patch("/list/:listId/visibility", authMiddleware, handleUpdateListVisibility);
+router.get('/lists', authMiddleware, handleGetOwnLists);
+router.get("/lists/public", handleGetPublicLists);
+router.get("/list/:listId/questions", handleGetQuestionsFromList);
 
 module.exports = router;
