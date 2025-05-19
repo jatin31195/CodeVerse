1b0f571ca6c56fe5cbf48841b2557d33cb1a7290
jatin31195 <jatinsingh31185@gmail.com>
2025-05-20 04:12:46 +0530

    UPDATE: updated controller as per storing/fetching EasyExplanation and realLifeExample of problem

--- Server/controllers/platformControllers/codeforces-controller.js
+++ Server/controllers/platformControllers/codeforces-controller.js
@@ -1,8 +1,21 @@
-const { codeforcesService, } = require('../../services/platformServices');
-const fetchDailyProblem = codeforcesService.fetchDailyProblem;
-const currentQues = codeforcesService.getTodayCodeforcesQuestions
 
-module.exports = {
-    fetchDailyProblem ,
-    currentQues
-};
+const { getCodeforcesPOTDByDate } = require("../../services/platformServices/codeforces-services");
+
+async function getPOTDByDate(req, res) {
+  try {
+    const { date } = req.params;
+    if (!date) {
+      return res.status(400).json({ success: false, message: "Date parameter is required" });
+    }
+
+    const question = await getCodeforcesPOTDByDate(date);
+    return res.status(200).json({ success: true, data: question });
+
+  } catch (err) {
+    console.error("❌ Codeforces POTD error:", err.message);
+    const code = err.statusCode || 500;
+    return res.status(code).json({ success: false, message: err.message });
+  }
+}
+
+module.exports = { getPOTDByDate };
