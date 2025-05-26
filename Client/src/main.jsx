6d0fea348d4e576fb84135b6e643eb75b0536696
jatin31195 <jatinsingh31185@gmail.com>
2025-05-27 00:35:12 +0530

    UPDATE: updated with google Login-signup functionality

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,15 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import './App.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import './App.css';
+import App from './App.jsx';
 import { Buffer } from 'buffer';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { SocketProvider } from './components/SocketContext.jsx';
+
 window.Buffer = Buffer;
-import { SocketProvider } from '../src/components/SocketContext.jsx';
+
+const GOOGLE_CLIENT_ID = '1047135861103-v89cl7qlfa7ka9jc68f2ufkg2s4665s8.apps.googleusercontent.com';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-     <SocketProvider>
-      <App />
-    </SocketProvider>
-  </StrictMode>,
-)
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <SocketProvider>
+        <App />
+      </SocketProvider>
+    </GoogleOAuthProvider>
+  </StrictMode>
+);
