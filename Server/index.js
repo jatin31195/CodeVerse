5515d5f5f2ba0cbe7b835efcdf8c196fe3de92e8
Jatin Rajput <84062894+jatin31195@users.noreply.github.com>
2025-06-01 15:45:54 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -20,7 +20,7 @@ const generalLimiter = rateLimit({
 app.use(generalLimiter);
 app.use(cors({
     origin: "http://localhost:5173", 
-    methods: "GET, POST, PUT, DELETE, PATCH",
+    methods: ["GET","POST","PUT","DELETE","PATCH","OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
 }));
