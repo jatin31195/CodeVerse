ef0c0b44f35f29cc1c6123e007933e17006ad4de
Jatin Rajput <84062894+jatin31195@users.noreply.github.com>
2025-09-02 21:32:40 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -19,7 +19,7 @@ dbConnection();
 //   legacyHeaders: false
 // });
 
-app.use(generalLimiter);
+// app.use(generalLimiter);
 const corsOptions = {
   origin: ["http://localhost:5173","https://code-verse-aonf.onrender.com","https://codeverse.solutions","https://www.codeverse.solutions"],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
